ae3f4fa7464f04fd8e77697b0a3e465c563b6d4e
Your Name <you@example.com>
2022-02-25 15:45:41 +0700

    fix bootstrap

--- toy-stores/src/views/Home.vue
+++ toy-stores/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>aaa</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
