4149741edb0c4094b2ce41c44c3b33cca2b0eef7
Longrui0911 <81362500+Longrui0911@users.noreply.github.com>
2022-02-26 08:05:42 +0700

    header&breadcrumbs

---
+++ toy-stores/src/components/layout/UserLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+<!-- Header  chua cat-->
+<slot> </slot>
+<the-footer />
+<the-last-footer />
+
+  </div>
+</template>
+
+<script>
+import TheFooter from '../common/TheFooter.vue'
+import TheLastFooter from '../common/TheLastFooter.vue'
+export default {
+  components: { TheFooter, TheLastFooter },
+name: 'UserLayout'
+}
+</script>
+
+<style>
+
+</style>
