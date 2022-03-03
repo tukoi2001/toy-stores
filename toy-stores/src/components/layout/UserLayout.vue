44dae99b0d0b24dfa435a2e4d04fe2f61e610c6c
Lê Văn Phong IT <mrbun1906@gmail.com>
2022-03-03 11:56:37 +0700

    merge sidebar.vue with me.vue

--- toy-stores/src/components/layout/UserLayout.vue
+++ toy-stores/src/components/layout/UserLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <the-header/>
+    <bread-crumbs :title="'My Account Page'"/>
     <slot> </slot>
     <the-footer />
     <the-last-footer />
@@ -8,11 +9,12 @@
 </template>
 
 <script>
+import BreadCrumbs from '../common/BreadCrumbs.vue'
 import TheFooter from '../common/TheFooter.vue'
 import TheHeader from '../common/TheHeader.vue'
 import TheLastFooter from '../common/TheLastFooter.vue'
 export default {
-  components: { TheFooter, TheLastFooter, TheHeader },
+  components: { TheFooter, TheLastFooter, TheHeader, BreadCrumbs },
 name: 'UserLayout'
 }
 </script>
---
+++ toy-stores/src/views/user/Tabs/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>A</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard"
+}
+</script>
+
+<style>
+
+</style>
