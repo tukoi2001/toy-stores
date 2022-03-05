0483dcbb22dd43ba08691678a645dd4c092e0663
Lê Văn Phong IT <mrbun1906@gmail.com>
2022-03-05 19:36:44 +0700

    fix home.vue conflict

---
+++ toy-stores/src/views/user/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <user-layout>
+      <slider />
+      <banners />
+      <featured-collection />
+    </user-layout>
+  </div>
+</template>
+
+<script>
+import Slider from "../../components/home/slider/Slider.vue";
+import Banners from "../../components/home/banners/Banners.vue";
+import FeaturedCollection from "../../components/home/featured-collection/FeaturedCollection.vue";
+import UserLayout from "../../components/layout/UserLayout.vue";
+export default {
+  name: "Home",
+  components: { UserLayout, Slider, Banners, FeaturedCollection },
+};
+</script>
+
+<style scoped src="@/assets/css/components/home/home.css">
+</style>
