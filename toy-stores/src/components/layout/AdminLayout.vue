8cd0c5ab44ecb825a190ee282566cc07a9445271
Your Name <you@example.com>
2022-03-06 20:57:59 +0700

    Done Side Bar Admin

--- toy-stores/src/components/layout/AdminLayout.vue
+++ toy-stores/src/components/layout/AdminLayout.vue
@@ -2,17 +2,22 @@
   <div>
     <header-admin />
     <div class="container-fluid">
-        
+        <div class="row">
+            <div class="col-sm-2 sidebar">
+                <side-bar-admin/>
+            </div>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
 import HeaderAdmin from "../admin/HeaderAdmin.vue";
+import SideBarAdmin from '../admin/SideBarAdmin.vue';
 export default {
-  components: { HeaderAdmin },
+  components: { HeaderAdmin, SideBarAdmin },
   name: "AdminLayout",
 };
 </script>
 
-<style scoped></style>
+<style scoped src="@/assets/css/components/admin/Admin.css"></style>
