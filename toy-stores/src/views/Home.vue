1fc4f4bcdd2e609587468a58cd1a775d9eb4ea2a
Lê Văn Phong IT <mrbun1906@gmail.com>
2022-03-07 20:44:09 +0700

    Update Home.vue

--- toy-stores/src/views/Home.vue
+++ toy-stores/src/views/Home.vue
@@ -3,18 +3,18 @@
     <user-layout>
       <slider />
       <banners />
-      <featured-collection/>
+      <featured-collection />
       <testimonials />
     </user-layout>
   </div>
 </template>
 
 <script>
-import Slider from '../components/home/slider/Slider.vue'
+import Slider from "../components/home/slider/Slider.vue";
 import Banners from "../components/home/banners/Banners.vue";
-import FeaturedCollection from '../components/home/featured-collection/FeaturedCollection.vue';
+import FeaturedCollection from "../components/home/featured-collection/FeaturedCollection.vue";
 import UserLayout from "../components/layout/UserLayout.vue";
-import Testimonials from '../components/home/testimonials/Testimonials.vue';
+import Testimonials from "../components/home/testimonials/Testimonials.vue";
 export default {
   name: "Home",
   components: { UserLayout, Slider, Banners, FeaturedCollection, Testimonials },
