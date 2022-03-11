9bb7a57972dff472909a2afbf6cf276ff868017a
Lê Văn Phong IT <mrbun1906@gmail.com>
2022-03-11 19:17:18 +0700

    Update Home.vue

--- toy-stores/src/views/Home.vue
+++ toy-stores/src/views/Home.vue
@@ -17,8 +17,8 @@ import Banners from "../components/home/banners/Banners.vue";
 import FeaturedCollection from "../components/home/featured-collection/FeaturedCollection.vue";
 import UserLayout from "../components/layout/UserLayout.vue";
 import Testimonials from '../components/home/testimonials/Testimonials.vue';
-import ProductList from '../components/home/product-list/ProductList.vue';
 import HomeBlogs from '../components/home/blogs/HomeBlogs.vue';
+import ProductList from '../components/home/product-list/ProductList.vue';
 export default {
   name: "Home",
   components: { UserLayout, Slider, Banners, FeaturedCollection, Testimonials, HomeBlogs, ProductList },
