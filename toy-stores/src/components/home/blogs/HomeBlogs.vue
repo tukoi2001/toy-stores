b25e59286387d34ff99e09b197df99a14202a570
Longrui0911 <81362500+Longrui0911@users.noreply.github.com>
2022-03-11 18:12:33 +0700

    blog-home

---
+++ toy-stores/src/components/home/blogs/HomeBlogs.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="section-padding">
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="section-title text-center aos-init aos-animate">
+              <h2 class="title">Testimonials</h2>
+              <p class="sub-title">What they say</p>
+            </div>
+            <div class="latest-blog-carousel">
+              <swiper class="swiper" :options="swiperOption">
+                <swiper-slide class="swiper-slide" v-for="(item, index) in listInfo" :key="index">
+                  <div class="single-blog">
+                    <div class="blog-thumb">
+                      <a href="">
+                        <img class="fit-image" :src="item.image" alt="">
+                      </a>
+                    </div>
+                    <div class="blog-content">
+                      <div class="blog-meta">
+                        <p>
+                          {{item.dateBlogs}}
+                          <span>{{item.author}}</span>
+                        </p>
+                      </div>
+                      <h5 class="blog-title">
+                        <a href="">{{item.blogTitle}}</a>
+                      </h5>
+                    </div>
+                  </div>
+                </swiper-slide>
+              </swiper>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      },
+      listInfo: [
+        {
+          image: require("@/assets/images/home/blog/1.jpg"),
+          dateBlogs: "03/11/2021 | ",
+          blogTitle: "It is a long established fact that a reader will be distracted",
+          author: "Admin",
+        },
+        {
+          image: require("@/assets/images/home/blog/2.jpg"),
+          dateBlogs: "03/11/2021 | ",
+          blogTitle: "It is a long established fact that a reader will be distracted",
+          author: "Admin",
+        },
+        {
+          image: require("@/assets/images/home/blog/3.jpg"),
+          dateBlogs: "03/11/2021 | ",
+          blogTitle: "It is a long established fact that a reader will be distracted",
+          author: "Admin",
+        },
+        {
+          image: require("@/assets/images/home/blog/1.jpg"),
+          dateBlogs: "03/11/2021 | ",
+          blogTitle: "It is a long established fact that a reader will be distracted",
+          author: "Admin",
+        },
+        {
+          image: require("@/assets/images/home/blog/2.jpg"),
+          dateBlogs: "03/11/2021 | ",
+          blogTitle: "It is a long established fact that a reader will be distracted",
+          author: "Admin",
+        },
+        
+      ],
+    };
+  },
+  props: {
+    image: {
+      type: String,
+      default: "",
+    },
+    dateBlogs: {
+      type: String,
+      default: "",
+    },
+    tblogTitle: {
+      type: String,
+      default: "",
+    },
+    author: {
+      type: String,
+      default: "",
+    },
+  },
+};
+
+</script>
+
+<style scoped src="@/assets/css/components/home/blogs.css">
+
+</style>
