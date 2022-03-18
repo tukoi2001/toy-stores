61a0b49fe658c93d37832eb9b7e1f674c78f35de
Longrui0911 <81362500+Longrui0911@users.noreply.github.com>
2022-03-18 19:32:47 +0700

    about-us page

--- toy-stores/src/views/user/About.vue
+++ toy-stores/src/views/user/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <the-header />
+    <bread-crumbs :title="'About page'"/>
+    <about-section-start />
+    <testimonials />
+    <our-team-about />
+    <the-footer />
+    <the-last-footer />
   </div>
 </template>
+
+<script>
+import AboutSectionStart from '../../components/about/AboutSectionStart.vue'
+import OurTeamAbout from '../../components/about/OurTeamAbout.vue'
+import BreadCrumbs from '../../components/common/BreadCrumbs.vue'
+import TheFooter from '../../components/common/TheFooter.vue'
+import TheHeader from '../../components/common/TheHeader.vue'
+import TheLastFooter from '../../components/common/TheLastFooter.vue'
+import Testimonials from '../../components/home/testimonials/Testimonials.vue'
+export default {
+  components: { TheHeader, BreadCrumbs, TheFooter ,TheLastFooter, AboutSectionStart, Testimonials, OurTeamAbout},
+
+}
+</script>
+
+<s,
+    TheLastFootertyle>
+
+</style>
